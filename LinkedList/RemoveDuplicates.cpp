4c74e75a005ba1b9e8132db05ffa9604812a1bb6
Priyanshi662 <priyanshiw567@gmail.com>
2023-01-21 23:06:38 +0530

    adding linked list problem

---
+++ LinkedList/RemoveDuplicates.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+class Solution {
+public:
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+            return head;
+        ListNode* p,*q;
+        p=head;
+        q=head->next;
+        // checking adjacent nodes and skipping the duplicate ones by adjusting the next pointer of previous node
+        while(q!=NULL && p->next!=NULL)
+        {
+            if(p->val==q->val)
+            {
+                q=q->next;
+                p->next=q;
+            }
+            else
+            {
+                p=p->next;
+                q=q->next;
+            }
+        }
+        return head;
+    }
+};
