a0795f3795c2edca0ae8c912a68b62231f16a490
Priyanshi662 <priyanshiw567@gmail.com>
2023-01-27 23:06:06 +0530

    Adding mergeSort

---
+++ LinkedList/mergeSortInLL.cpp
@@ -0,0 +1,3 @@
+// Heap Sort cannot be used in Linked List because it is not a random access data structure.
+// Merge Sort is used in linked list because other sorting work slow for linked list
+// 
