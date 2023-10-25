74e6b7f3cdef38ec4ae9061579f7a465098d354e
Priyanshi662 <priyanshiw567@gmail.com>
2023-10-25 22:59:22 +0530

    adding logical thinking

---
+++ 14-Logical Thinking/01-KthSymbolInGrammar.cpp
@@ -0,0 +1,25 @@
+// Leetcode - 779
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n==1)
+            return 0;
+        if(k%2==0)
+        {
+            // right child
+            if(kthGrammar(n-1,k/2)==0)
+                return 1;
+            else
+                return 0;
+        }
+        else
+        {
+            // left child
+            if(kthGrammar(n-1,(k+1)/2)==0)
+                return 0;
+            else
+                return 1;
+        }
+
+    }
+};
