ae0e32000579f5031860c2ed50456337e87f595a
Priyanshi662 <priyanshiw567@gmail.com>
2023-10-29 00:28:25 +0530

    modifying solution

--- 07-Graph/03-CheckCycleInDirectedGraph.cpp
+++ 07-Graph/03-CheckCycleInDirectedGraph.cpp
@@ -1,41 +1,68 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 // we need to use backtracking because their will be forward edges in a loop and backward edges to bring it back to a single point
-// cannot use bfs because path is required to be checked
 // Check for back edges which lead upto the ancestor node of the current node
 
-bool dfs(vector<int> adj[],int v,int src,vector<bool> &dfsvis,vector<bool> &vis)
+// Using BFS:
+bool bfs(vector<int> adj[],int v,int src,vector<bool> &vis)
 {
-    dfsvis[src]=true;
+    vector<int> parent(v,-1);
+    queue<int> q;
+    q.push(src);
+    parent[src]=-1;
+    while(!q.empty())
+    {
+        int curr=q.front();
+        q.pop();
+        for(auto it: adj[curr])
+        {
+            if(vis[it]==true&& it!=parent[curr])
+            {
+                // it was visited without the parent
+                return true;
+            }
+            else
+            {
+                vis[it]=true;
+                q.push(it);
+                parent[it]=curr;
+            }
+        }
+    }
+    return false;
+}
+bool dfs(vector<int> adj[],int v,int src,int parent,vector<bool> &vis)
+{
     vis[src]=true;
+    
     for(auto it: adj[src])
     {
         if(!vis[it])
         {
-            bool res=dfs(adj,v,it,dfsvis,vis);
+            bool res=dfs(adj,v,it,src,vis);
             if(res)
                 return true;
         }
-        else if(dfsvis[it])
+        else if(parent!=it)
         {
-            // dfsvis[i] is true when this particular node has been visited in the current DFS iteration
             return true;
         }
     }
-    dfsvis[src]=false;
+    
     return false;
 }
 bool checkCycle(vector<int> adj[],int V)
 {
-    vector<bool> dfsvis(V,false);
     vector<bool> vis(V,false);
     for(int i=0;i<V;i++)
     {
+        // for disconnected components:
         if(vis[i]==false)
         {
-            if(dfs(adj,V,i,dfsvis,vis))
+            if(dfs(adj,V,i,-1,vis))
                 return true;
         }
     }
